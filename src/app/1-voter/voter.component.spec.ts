b10a7150e121a0a32b86ad73a612a004d3fd6fd6
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-12 23:01:38 +0530

    Basic setup to create test cases
    TestBed.configureTestingModule
    ComponentFixture

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,8 +1,20 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { VoterComponent } from './voter.component';
 
 describe('VoterComponent', () => {
 
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement - returns HTML element which is the root DOM element for this component's template
+    // fixture.debugElement - wrapper around nativeElement. provides some useful methods
   });
 
   it('', () => {
