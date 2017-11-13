514b9c0b5b8c66476509596e0cc7b32dc2c5083b
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-14 00:14:12 +0530

    Testing Attribute Directives

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -4,6 +4,8 @@ import { HighlightDirective } from './highlight.directive';
 import { By } from '@angular/platform-browser';
 import { Component } from '@angular/core'; 
 
+// When testing attribute directive, you should create a host component in your test to test various
+//    usages of that attribute
 @Component({
   template: `
     <p highlight="cyan">First</p>
@@ -18,6 +20,8 @@ describe('HighlightDirective', () => {
   
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      // Need to add both component and directive in declarations
+      // If directive is not added, that it won't be applied, bcoz angular will not recognize it
       declarations: [ DirectiveHostComponent, HighlightDirective ]
     })
     .compileComponents();
@@ -26,5 +30,18 @@ describe('HighlightDirective', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DirectiveHostComponent);
     fixture.detectChanges(); 
+  });
+
+  it('should highlight the first element with cyan', () => {
+    let de = fixture.debugElement.queryAll(By.css('p'))[0];
+
+    expect(de.nativeElement.style.backgroundColor).toBe('cyan');
+  });
+
+  it('should highlight the second element with default color', () => {
+    let de = fixture.debugElement.queryAll(By.css('p'))[1];
+    let directive = de.injector.get(HighlightDirective);
+
+    expect(de.nativeElement.style.backgroundColor).toBe(directive.defaultColor);
   });
 });
